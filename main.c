bbe5f45c7aaa48529c9d165cb6704e3115bedb57
Avi Soboroff <avi.soboroff@gmail.com>
2025-12-28 17:41:16 -0500

    basic main loop done

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]){
 
     char line[1024];
     char *tokens[255]; //array for tokens/commands
-    int ntok;
+    int ntok; //number of tokens in command line
     char *cmd;
 
     while(1){
@@ -37,6 +37,20 @@ int main(int argc, char *argv[]){
         }
         else if (strcmp("jobs", cmd)==0){
             printf("0 total jobs\n");
+        }
+        else if (strcmp("pause", cmd)==0){
+            double time = atof(tokens[1]);
+            pause_for(time);
+            printf("pausing for %.3f seconds\n", time);
+        }
+        else if (strcmp("tokens", cmd)==0){
+            printf("%d tokens in command line\n", ntok);
+            for (int i = 0; i < ntok; i++){
+                printf("tokens[%d]: %s\n", i, tokens[i]);
+            }
+        }
+        else if (strcmp("wait", cmd)==0){
+            printf("ERROR: No job '%s' to wait for\n", tokens[1]);
         }
     }
 
